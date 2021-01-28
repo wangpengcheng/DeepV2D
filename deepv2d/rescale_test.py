63e53b3c83ddbe29e1e0fd12b14548113f37920f
wangpengcheng <wangpengcheng2018@gmail.com>
2021-01-28 11:14:19 +0800

    添加新数据

--- deepv2d/rescale_test.py
+++ deepv2d/rescale_test.py
@@ -3,7 +3,7 @@ from geometry.intrinsics import *
 from PIL import Image
 import matplotlib.pyplot as plt
 from geometry.projective_ops import *
-
+from geometry.se3 import *
 
 import torch 
 
@@ -31,5 +31,15 @@ def test_projective():
     #coords =  project(a,b)
     coo1 = backproject(a,b)
 
+def test_se3():
+    # image=Image.open("/home/node/workspace/DeepV2D/depth.png").convert('RGB')
+    # print(image.size, image.format, image.mode)
+    # tensorSrc = transforms.ToTensor()(image)
+    #tensorSrc = torch.stack((tensorSrc,tensorSrc),dim=0)
+    a = torch.randn(3,3)
+    b = torch.randn(1,3)
+    #coords =  project(a,b)
+    coo1 = matdotv(a,b)
+    print(coo1)
 if __name__ == '__main__':
-    test_projective()
+    test_se3()
