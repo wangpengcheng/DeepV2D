a48400d221f269b62c926c18b17df24245d510c0
wangpengcheng <wangpengcheng2018@gmail.com>
2021-01-30 21:30:22 +0800

    添加测试脚本

---
+++ script/eval.sh
@@ -0,0 +1,3 @@
+
+n ./evaluation/eval_tum.py --cfg=cfgs/tum.yaml  --dataset_dir=data/tum --model=checkpoints/tum/tmu_model/_stage_2.ckpt
+
---
+++ script/train_tum.sh
@@ -0,0 +1,1 @@
+python ./training/train_tum.py --cfg=cfgs/tum.yaml --name=tmu_model --dataset_dir=data/tum --restore=checkpoints/tum/tmu_model/_stage_2.ckpt
