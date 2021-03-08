0efccd021bf8c255cfe194dc53f7e3842afb58d9
wangpengcheng <wangpengcheng2018@gmail.com>
2021-03-08 18:58:52 +0800

    添加数据

--- script/nyu/train_nyu_resnet_resnet.sh
+++ script/nyu/train_nyu_resnet_resnet.sh
@@ -1,3 +1,3 @@
 # nyu 训练执行脚本
 # 实验1 
-python ./training/train_nyuv2.py --cfg=cfgs/nyu/nyu_2_2_resnet_resnet.yaml --name=nyu_model --dataset_dir=data/nyu2 --tfrecords=data/nyu_train/nyu_train1.tfrecords --restore=checkpoints/tum/tmu_model/_stage_2.ckpt
+python ./training/train_nyuv2.py --cfg=cfgs/nyu/nyu_2_2_resnet_resnet.yaml --name=nyu_model --tfrecords=data/nyu_train/nyu_train.tfrecords --restore=checkpoints/tum/tmu_model/_stage_2.ckpt
