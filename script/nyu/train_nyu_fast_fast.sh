0134ce39fb1a63d5b18717c9dc1937f0bf1ecda1
wangpengcheng <wangpengcheng2018@gmail.com>
2021-03-12 21:30:25 +0800

    数据合并

--- script/nyu/train_nyu_fast_fast.sh
+++ script/nyu/train_nyu_fast_fast.sh
@@ -1,7 +1,3 @@
 # nyu 训练执行脚本
 # 实验 2
-<<<<<<< HEAD
-python ./training/train_nyuv2.py --cfg=cfgs/nyu/nyu_2_2_fast_fast.yaml --name=nyu_model  --tfrecords=data/nyu/nyu_train.tfrecords --restore=checkpoints/tum/tmu_model/_stage_2.ckpt
-=======
 python ./training/train_nyuv2.py --cfg=cfgs/nyu/nyu_2_2_fast_fast.yaml --name=nyu_model --tfrecords=data/nyu/nyu_train1.tfrecords --restore=checkpoints/tum/tmu_model/_stage_2.ckpt
->>>>>>> f92ed8d63686bd5a76addd5d55997a6c904aaf72
