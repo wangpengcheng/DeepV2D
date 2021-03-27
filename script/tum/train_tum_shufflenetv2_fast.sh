6c3add276e079afd3356a0e08ec4a68369ff9394
wangpengcheng <wangpengcheng2018@gmail.com>
2021-03-27 22:26:20 +0800

    进行模型训练

--- script/tum/train_tum_shufflenetv2_fast.sh
+++ script/tum/train_tum_shufflenetv2_fast.sh
@@ -1,3 +1,3 @@
 # nyu 训练执行脚本
 # 实验编号 8 
-python ./training/train_tum.py --cfg=cfgs/tum/tum_2_2_shufflev2_fast.yaml --dataset_dir=data/tum   --name=nyu_model  --restore=checkpoints/tum/tmu_model/_stage_2.ckpt
+python ./training/train_tum01.py --cfg=cfgs/tum/tum_2_2_shufflev2_fast.yaml --dataset_dir=data/tum   --name=nyu_model  --restore=checkpoints/tum/tmu_model/_stage_2.ckpt
